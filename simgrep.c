db5e1ea89bc2aa4fc86603a7c599be3f6e1c9c7b
pedrofixe <yurbypedro@hotmai.com>
2018-03-21 16:01:11 +0000

    Started working on arguments. Started strContains function

--- simgrep.c
+++ simgrep.c
@@ -7,13 +7,43 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#define BUFFER_SIZE 128
+#define BUFFER_SIZE 512
 
 
-
-int noOptions(int argc, char const *argv[]) // 
+char* strContains(const char* str1, const char* str2, const int arguments[]) // Searches for str2 in str1
 {
-	FILE *file = fopen(argv[2], "r");
+	int found = 1;
+
+	int i;
+	for (i = 0; i < strlen(str1); ++i)
+	{
+		found = 1;
+
+		for (int j = 0; j < strlen(str1); ++j)
+		{
+			if (arguments[0]) // -i maiusculas
+			{
+
+			}
+			else
+			{
+				if (str1[i+j] != str2[j])
+				{
+					found = 0;
+					break;
+				}
+			}
+		}
+
+		if (found) // Found
+			return str[i];
+	}
+}
+
+
+int noOptions(const char* pattern, const char* filename, const int arguments[])
+{
+	FILE *file = fopen(filename, "r");
 
 	if (file == NULL)
 	{
@@ -29,7 +59,7 @@ int noOptions(int argc, char const *argv[]) //
 	{
 		numRead = getline(&buffer, &bufferSize, file);
 		
-		if (strstr(buffer, argv[1]) != NULL)
+		if (strstr(buffer, pattern) != NULL)
 		{
 			printf("%s", buffer);
 		}
@@ -46,9 +76,17 @@ int noOptions(int argc, char const *argv[]) //
 int main(int argc, char const *argv[])
 {
 
+	int arguments[6] = {0,0,0,0,0,0}; // 0 = i, 1 = l, 2 = n, 3 = c, 4 = w, 5 = r
+
+	int i;
+	for (i = 0; i < argc; ++i)
+	{
+		if (strcmp())
+	}
+
 	if (argc == 3)
 	{
-		return noOptions(argc, argv);
+		return noOptions(argv[1], argv[2], arguments);
 	}
 	else
 	{
