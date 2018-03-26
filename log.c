0d2f22c785ccad08eef0e779b401a4f17222043b
pedrofixe <yurbypedro@hotmai.com>
2018-03-26 12:59:06 +0100

    Almost done implementing log feature

--- log.c
+++ log.c
@@ -1,2 +1,29 @@
 #include "log.h"
 
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+
+clock_t beginning;
+FILE *file;
+
+void start(const char* filename)
+{
+	beginning = clock();
+	file = fopen(filename, "a");
+}
+
+long double getTime(void)
+{
+	return (long double)(clock() - beginning) / CLOCKS_PER_SEC;
+}
+
+int logPrint(const char* output)
+{
+	int res = fprintf(file, "%.2Lf - %u - %s\n", getTime()*1000, getpid(), output);
+
+	if (res < 0)
+		printf("Error writing to log file!\n");
+
+	return res;
+}
--- log.h
+++ log.h
@@ -1,9 +1,11 @@
 #ifndef LOG_H
 #define LOG_H
 
+void start(const char* filename);
 
+long double getTime(void);
 
-
+int logPrint(const char* output);
 
 
 #endif
