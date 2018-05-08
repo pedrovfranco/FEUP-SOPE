17239cb28d1bd28c7932591ac4a5de271f356050
ff7 <francisco.tf4@gmail.com>
2018-05-08 14:27:32 +0100

    Updated the given files

--- Projeto_Final/client_v2.c
+++ Projeto_Final/client_v2.c
@@ -6,8 +6,8 @@
 int main(int argc, char *argv[]) {
   printf("** Running process %d (PGID %d) **\n", getpid(), getpgrp());
 
-  if (argc == 3)
-    printf("ARGS: %s | %s\n", argv[1], argv[2]);
+  if (argc == 4)
+    printf("ARGS: %s | %s | %s\n", argv[1], argv[2], argv[3]);
 
   sleep(1);
 
